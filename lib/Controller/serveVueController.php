3ece8225846b1bcdba8d2b2c1fd0f9ccb8154f93
YourLittleHelper <oskars@shibetec.com>
2018-05-24 12:58:03 +0300

    Add vuejs new config.

--- lib/Controller/serveVueController.php
+++ lib/Controller/serveVueController.php
@@ -3,7 +3,14 @@
 class serveVueController {
 
     public function doAction() {
-        include('public/vue_dist/index.html');
+        // TODO INJECT CONFIG INTO index.html
+        $config = new \stdClass();
+        $config->baseUrl = INIT::$RELATIVE_HOST_NAME;
+        $config->googleClientId = INIT::$OAUTH_CLIENT_ID;
+        $index = file_get_contents('public/vue_dist/index.html');
+        $index = str_replace('HugoCat.main({})', 'HugoCat.main('. json_encode($config) .')', $index);
+        $index = str_replace('<baseUrl>', $config->baseUrl, $index);
+        echo $index;
     }
 
     public function finalize() {}
