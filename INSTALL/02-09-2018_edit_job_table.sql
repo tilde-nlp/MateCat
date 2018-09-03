b0d2d147f84f0796d551833dc94890901e9ca702
YourLittleHelper <oskars@shibetec.com>
2018-09-03 03:17:06 +0300

    Add script fix.

--- INSTALL/02-09-2018_edit_job_table.sql
+++ INSTALL/02-09-2018_edit_job_table.sql
@@ -1,4 +1,4 @@
-USE 'matecat';
+USE `matecat`;
 
 ALTER TABLE `jobs`
-	ADD COLUMN ``editing_time` BIGINT NOT NULL DEFAULT '0' AFTER `total_raw_wc`;
+	ADD COLUMN `editing_time` BIGINT NOT NULL DEFAULT '0' AFTER `total_raw_wc`;
