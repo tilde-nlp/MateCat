b9987b73f80d7ebfc9d7ee87203c92bcfede14e4
YourLittleHelper <oskars@shibetec.com>
2018-12-21 16:09:05 +0200

    Add nocache to urls request.

--- lib/Controller/API/V2/UrlsController.php
+++ lib/Controller/API/V2/UrlsController.php
@@ -20,6 +20,11 @@ class UrlsController extends KleinController {
     private $validator;
 
     public function urls() {
+        header("Expires: Tue, 03 Jul 2001 06:00:00 GMT");
+        header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+        header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+        header("Cache-Control: post-check=0, pre-check=0", false);
+        header("Pragma: no-cache");
 
         $this->featureSet->loadForProject( $this->validator->getProject() );
 
