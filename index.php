06b14042ac1de745e3bd26f112a00bd6ab737ada
YourLittleHelper <oskars@shibetec.com>
2020-07-09 09:42:21 +0300

    Change server setup, serving all data from public folder only.

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-if( !@include_once 'inc/Bootstrap.php')
-	header("Location: configMissing");
-	
-Bootstrap::start();
-
-$controller = controller::getInstance ();
-$controller->doAction ();
-$controller->finalize ();
---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+if( !@include_once '../inc/Bootstrap.php')
+	header("Location: configMissing");
+
+Bootstrap::start();
+
+$controller = controller::getInstance ();
+$controller->doAction ();
+$controller->finalize ();
